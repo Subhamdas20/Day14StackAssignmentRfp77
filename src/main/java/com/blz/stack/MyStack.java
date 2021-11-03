3322cc80c3e03553a480fd35e98b63ade7a12f7f
Subhamdas20 <subham21.sd@gmail.com>
2021-11-04 01:23:10 +0530

    Added : uc1 to create a stack to push elements

--- src/main/java/com/blz/stack/MyStack.java
+++ src/main/java/com/blz/stack/MyStack.java
@@ -1,35 +1,29 @@
 package com.blz.stack;
-
 import com.bz.linkedlist.MyLinkedList;
 import com.bz.linkedlist.INode;
 import com.bz.linkedlist.MyNode;
-
 public class MyStack {
     /*
-     * To create stack operations
-     * */
+    * To create stack operations
+    * */
 
-    private MyLinkedList myLinkedList;
-
+    private  MyLinkedList myLinkedList;
     /*
-     *  Constructor
-     */
+    *  Constructor
+    */
     public MyStack() {
         this.myLinkedList = new MyLinkedList();
     }
-
-    /*
-     * used to push nodes
-     */
-    public void push(INode myNode) {
+/*
+* used to push nodes
+*/
+    public void push(INode myNode){
         myLinkedList.add(myNode);
 
     }
-
-    /*
-     * used to delete nodes
-     */
-
+    public INode peak(){
+        return myLinkedList.head;
+    }
     public void printStack() {
         myLinkedList.printNodes();
     }
